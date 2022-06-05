04363922fa3eff85bd9f17b244f9567c689d83e2
sungchol <56203063+sungchol@users.noreply.github.com>
2022-06-05 17:46:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,207 @@
-const playerText = document.querySelector("#playerText");
-const computerText = document.querySelector("#computerText");
-const resultText = document.querySelector("#resultText");
-const choiceBtns = document.querySelectorAll(".choiceBtn");
+const cellContainer = document.querySelector("#cellContainer");
 
-let player, computer, result;
-
-choiceBtns.forEach(button => button.addEventListener("click", 
-    () => {
-        player = button.textContent;
-        computerTurn();
-        playerText.textContent = `Player: ${player}`;
-        computerText.textContent = `Computer: ${computer}`;
-        resultGame();
-        resultText.textContent = `Result: ${result}`;
-    }
-));
-
-function computerTurn() {
-
-    const randNum = Math.floor(Math.random()*3) +1;
-
-    switch(randNum) {
-        case 1:
-            computer = "ROCK";
-            break;
-        case 2:
-            computer = "PAPER";
-            break;
-        case 3:
-            computer = "SCISSORS";
-            break;
-
-    }
+//오목 셀만들기
+for(let i =0 ; i< 400; i++)
+{
+   const myDiv = document.createElement("div");
+   myDiv.setAttribute("cellIndex", `${i}`);
+   myDiv.setAttribute("class", "cell");
+   cellContainer.appendChild(myDiv);
 }
 
-function resultGame() {
-    if(player == 'ROCK') {
-        if(computer == 'ROCK') {
-            result = "Tie";    
-        } else if(computer == 'PAPER') {
-            result = "You Win";    
-        } else if(computer == 'SCISSORS') {
-            result = "You Lose";    
-        }
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restart");
+const backBtn = document.querySelector("#back");
+
+let options = new Array();
+let posOrder = new Array(); //놓은 순서기억 push, pop
+let currentPlayer = "●";
+let running = false;
+
+initializeGame();
+
+function initializeGame() {
+    cells.forEach(cell => 
+        cell.addEventListener("click", cellClicked));
+    restartBtn.addEventListener("click", restartGame);
+    backBtn.addEventListener("click", backGame);
+    statusText.textContent = `${currentPlayer}'s turn`;
+    
+    for(let i =0 ; i<400; i++) {
+        options[i] = "";
+    }
+    running = true;
+}
+
+function cellClicked() {
+    const cellIndex = this.getAttribute("cellIndex");
+
+    if(options[cellIndex] != "" || !running) {
+        return;
     }
 
-    else if(player == 'PAPER') {
-        if(computer == 'ROCK') {
-            result = "You Win";
-        } else if(computer == 'PAPER') {
-            result = "Tie";    
-        } else if(computer == 'SCISSORS') {
-            result = "You Lose";    
+    updateCell(this, cellIndex);
+    checkWinner();
+    //console.log(posOrder);
+}
+
+function updateCell(cell, index) {
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+    const order = {player: currentPlayer, index: index};
+    posOrder.push(order);
+}
+
+function changePlayer(){
+    currentPlayer = (currentPlayer == "●") ? "◎" : "●";
+    statusText.textContent = `${currentPlayer}'s turn`;
+}
+
+function checkWinner()
+{
+    let roundWon = false;
+
+    for(let y=0; y<20; y++)
+    {
+        for(let x=0 ; x<20; x++) {
+            //검사위치 값이 현player && (x=0 또는 (x-1) != 현플레이어)
+            if(options[y*20+x] == currentPlayer) {
+                
+                //경우1 : x축방향으로 쭉이어진경우
+                if(x==0 || (x>0 && options[y*20+x-1] != currentPlayer)) {
+                    //x축이동공간이 5칸 || x축이동공간 6칸이상이면서 (6칸) != 현플레이어
+                    if(x==15 || (x<15 && options[y*20+x+5] !=currentPlayer))
+                    {
+                        //4칸연속 현플레이어이면 승리조건충족
+                        roundWon = true;
+                        for(let i=0; i<4;i++) {
+                            if(options[y*20+x+i+1] != currentPlayer) {
+                                roundWon = false;
+                                break;
+                            }    
+                        }
+
+                        if(roundWon){
+                            break;
+                        }
+                    }
+                }
+
+                //경우2 : y축방향으로 쭉이어진경우
+                if(y==0 || (y>0 && options[(y-1)*20+x] != currentPlayer)) {
+                    //y축이동공간이 5칸 or y축이동공간 6칸이상이면서 (6칸) != 현플레이어
+                    if(y==15 || (y<15 && options[(y+5)*20+x] !=currentPlayer))
+                    {
+                        //4칸연속 현플레이어이면 승리조건충족
+                        roundWon = true;
+                        for(let i=0; i<4;i++) {
+                            if(options[(y+i+1)*20+x] != currentPlayer) {
+                                roundWon = false;
+                                break;
+                            }    
+                        }
+
+                        if(roundWon){
+                            break;
+                        }
+                    }
+                }
+
+                //경우3 : 45도 방향으로 쭉 이어진경우
+                if(x==0 || y==0 || (x>0 && y>0 && options[(y-1)*20+(x-1)] != currentPlayer)) {
+                    //y축이동공간이 5칸 || y축이동공간 6칸이상이면서 (6칸) != 현플레이어
+                    if(x==15 || y==15 || (y<15 && x<15 && options[(y+5)*20+(x+5)] !=currentPlayer))
+                    {
+                        //4칸연속 현플레이어이면 승리조건충족
+                        roundWon = true;
+                        for(let i=0; i<4;i++) {
+                            if(options[(y+i+1)*20+(x+i+1)] != currentPlayer) {
+                                roundWon = false;
+                                break;
+                            }    
+                        }
+
+                        if(roundWon){
+                            break;
+                        }
+                    }
+                }
+
+                //경우4 : 135도 방향으로 쭉 이어진경우
+               if(x==19 || y==19 || (x<19 && y<19 && options[(y-1)*20+(x+1)] != currentPlayer)) {
+                    //y축이동공간이 5칸 || y축이동공간 6칸이상이면서 (6칸) != 현플레이어
+                    if(x==4 || y==4 || (y>4 && x>4 && options[(y+5)*20+(x-5)] !=currentPlayer))
+                    {
+                        //4칸연속 현플레이어이면 승리조건충족
+                        roundWon = true;
+                        for(let i=0; i<4;i++) {
+                            if(options[(y+i+1)*20+(x-i-1)] != currentPlayer) {
+                                roundWon = false;
+                                break;
+                            }    
+                        }
+
+                        if(roundWon){
+                            break;
+                        }
+                    }
+                }
+            }
         }
+
+        if(roundWon) {
+            break;
+        }
+        
     }
 
-    else if(player == 'SCISSORS') {
-        if(computer == 'ROCK') {
-            result = "You Lose";
-        } else if(computer == 'PAPER') {
-            result = "You Win";    
-        } else if(computer == 'SCISSORS') {
-            result = "Tie";    
+    if(roundWon) {
+        statusText.textContent = `${currentPlayer} wins!!`;
+        running = false;
+    }
+    else if(!options.includes("")){
+        statusText.textContent = `Draw!!`;
+    }
+    else {
+        changePlayer();
+    }
+
+}
+
+//재시작, 변수초기화
+function restartGame() {
+    currentPlayer = "●";
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = ``);
+    for(let i =0 ; i<400; i++) {
+        options[i] = "";
+    }
+    //놓은 순서기억공간 초기화
+    posOrder.length = 0;
+    running = true;
+}
+
+//한단계 뒤로 이동하기 posOrder 스택 pop
+function backGame() {
+    const count = posOrder.length;
+    if(count > 0) {
+        const pos = posOrder.pop();
+
+        options[pos.index] = "";
+        
+        //화면지우기
+        for(let i = 0; i < cells.length; i++) {
+            const cell = cells[i];
+            if(cell.getAttribute("cellIndex") == pos.index) {
+                cell.textContent = "";
+                break;
+            };
         }
+
+        running = true;
+        //changePlayer();
+        currentPlayer=pos.player;
+        statusText.textContent = `${currentPlayer}'s turn`;
     }
 }
